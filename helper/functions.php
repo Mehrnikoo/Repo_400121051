753cd26c6b534ad40ac4f008d0082eefbb406bd4
Parmida <Mehrnikoo.net@gmail.com>
2025-05-23 23:10:40 +0330

    another changes

--- helper/functions.php
+++ helper/functions.php
@@ -1,11 +1,5 @@
 <?php
 function view($filename){
-    $path = __DIR__ . '/../view/' . $filename . '.php';
-    if (file_exists($path)) {
-        include $path;
-    } else {
-        echo "View not found: " . $filename;
-    }
+    return require_once(__DIR__ . '/../view/' . $filename . '.php');
 }
-
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 // filepath: /home/glitching0ut/Documents/university/projects/index.php
+define('BASE_PATH', dirname(__DIR__));
+
+require_once BASE_PATH . '/vendor/autoload.php';
 
 //echo "<h1>Welcome to Your Web Programming Journey!</h1>";
 //echo "<p><a href='public/read.php'>View Records</a></p>";
