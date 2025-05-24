81b6f44d67f8401ae0f5fc6d314a53f8d6af7dda
Parmida <Mehrnikoo.net@gmail.com>
2025-05-24 15:37:02 +0330

    new test

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <?php
 // filepath: /home/glitching0ut/Documents/university/projects/index.php
-define('BASE_PATH', dirname(__DIR__));
+// define('BASE_PATH', dirname(__DIR__));
 
-require_once BASE_PATH . '/vendor/autoload.php';
+// require_once BASE_PATH . '/vendor/autoload.php';
 
-//echo "Welcome to Your Web Programming Journey!";
-//echo "<p><a href='public/read.php'>View Records</a></p>";
-$request = new Request(
+echo "Welcome to Your Web Programming Journey!";
+// //echo "<p><a href='public/read.php'>View Records</a></p>";
+// $request = new Request(
 
-    $_GET,
-    $_POST,
-    $_SERVER,
-    $_FILES,
-    $_COOKIE
+//     $_GET,
+//     $_POST,
+//     $_SERVER,
+//     $_FILES,
+//     $_COOKIE
 
-);
+// );
 
 
-//HENDLER
-$kernel = new Kernel(
-    $request,
-    new Router(),
-    new Response(),
-    new View()
-);
+// //HENDLER
+// $kernel = new Kernel(
+//     $request,
+//     new Router(),
+//     new Response(),
+//     new View()
+// );
 
-$response = $kernel->handle($request);
-//SEND RESPONSE
-$response->send();
+// $response = $kernel->handle($request);
+// //SEND RESPONSE
+// $response->send();
 
 
 
